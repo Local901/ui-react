c0179222a65f399188392d9e2859d8b40119c29c
Local901 <135715189+Local901@users.noreply.github.com>
2026-02-14 14:29:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# template library
+
+This is a template js library. Make sure to correctly fill in the `package.json` file.
---
+++ tests/index.test.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from "vitest";
+
+describe("Validator", () => {
+    it("Should not throw an error when valid", () => {
+        expect(true).toBeTruthy();
+    });
+});
